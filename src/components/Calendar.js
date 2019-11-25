be81ddf9244fb0878058781e899f17ccc6251b0b
Ken Coppola <kcoppola@ranker.com>
2019-11-25 14:40:01 -0500

    At this point the calendar  displays the correct number of days in a month. I need to place the date on  the  correct day though. The day can be selected and the displayed date  will change.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,22 +5,25 @@ import '../css/calendar.css';
 const Calendar = () => {
 
     const date = new Date();
-    let today = date.toDateString().slice(4);
-    const day = date.getDate();
+
+
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     let [month, setMonth] = useState(date.getMonth());
     let [displayMonth, setDisplayMonth] = useState(months[month]);
+    let [currentMonth, setCurrentMonth] = useState(months[date.getMonth()]);
+    let [currentYear, setCurrentYear] = useState(date.getFullYear());
     let [year, setYear] = useState(date.getFullYear());
     let [displayYear, setDisplayYear] = useState(year);
     let [showCalendar, setShowCalendar] = useState(false);
+    let [displayedDay, setDisplayedDay] = useState(date.getDate());
+    let [selectedDay, setSelectedDay] = useState();
 
 
+    console.log(date.getDay());
     const getDate = (month, year) => new Date(year, month, 0).getDate();
 
     let [days, setDays] = useState(getDate(month, year));
-    let dateSelected = date;
-    let daySelected = day;
     let arrayOfDays = [];
 
     for (let i = 1; i <= days; i++) {
@@ -28,7 +31,7 @@ const Calendar = () => {
     }
 
     let previousMonth = () => {
-
+        setSelectedDay('')
         if (month === 0) {
             month = 12;
             setYear(year--);
@@ -39,12 +42,9 @@ const Calendar = () => {
         setMonth(month--);
         setDisplayMonth(months[month]);
         setDays(getDate(month + 1, year));
-
     }
-
-
     let nextMonth = () => {
-
+        setSelectedDay('')
         if (month === 11) {
             month = -1;
             setYear(year++);
@@ -63,9 +63,20 @@ const Calendar = () => {
 
     return (
         <div className="date-picker">
-            <div className="selected-date" onClick={showTheCalendar}>{today}</div>
+            <div className="selected-date" onClick={showTheCalendar}>{!selectedDay ? <span>{currentMonth} {displayedDay} {currentYear}</span> : <span>{displayMonth} {selectedDay} {year}</span>}</div>
 
-            {showCalendar ? <><div className="dates"><div className="month"><div className="arrows previous" onClick={previousMonth}>&lt;</div><div className="displayMonth">{displayMonth} {displayYear}</div><div className="arrows nextMonth" onClick={nextMonth}>&gt;</div></div><div className="days">{arrayOfDays.map((day, index) => <div key={index}>{day}</div>)}</div></div></> : ''}
+            {showCalendar ?
+                <><div className="dates">
+                    <div className="month">
+                        <div className="arrows previous" onClick={previousMonth}>&lt;</div>
+                        <div className="displayMonth">{displayMonth} {displayYear}</div>
+                        <div className="arrows nextMonth" onClick={nextMonth}>&gt;</div>
+                    </div>
+                    <div className="days">{arrayOfDays.map((day, index) => <div key={index} onClick={() => setSelectedDay(index + 1)}>{day}</div>)}
+                    </div>
+                </div>
+                </>
+                : ''}
         </div>
     )
 }
