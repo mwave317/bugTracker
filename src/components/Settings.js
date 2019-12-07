1384fe8b2b56e248955949f16dde5a1e5b13d33d
Ken Coppola <kcoppola@ranker.com>
2019-12-06 23:09:01 -0500

    Wanted to fix the formatting and reomve the console.log statement.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,29 +3,26 @@ import axios from 'axios';
 import '../App.css';
 
 const Settings = () => {
-    const [image, setImage ] = useState('');
-    const [loading, setLoading ] = useState(false);
+    const [image, setImage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const uploadImage = e => {
         const files = e.target.files[0];
         const formData = new FormData();
-              formData.append("upload_preset", "bugtracker1");
-              formData.append("file", files);              
+        formData.append("upload_preset", "bugtracker1");
+        formData.append('file', files);
         setLoading(true);
 
-         axios.post('https://api.cloudinary.com/v1_1/bugtracker/image/upload', formData )
-        .then(response => setImage(response.data.secure_url))
-        .then(response => console.log(response))
-        .then(setLoading(false))
-        .catch(err => console.log('This is from the file upload', err))
+        axios.post('https://api.cloudinary.com/v1_1/bugtracker/image/upload', formData)
+            .then(res => setImage(res.data.secure.url))
+            .then(setLoading(false))
+            .catch(err => console.log(err))
     }
 
     return (
         <div>
-<input className="settings-image" type="file" name="file" placeholder="upload a new image" onChange={uploadImage}/>
-
-{loading ? (<h3>Loading ....</h3>) : (<img className="image" src={image}/>)}
-
+            <input className="settings-image" type="file" name="file" onChange={uploadImage} />
+                {loading ? <h1>Loading....</h1> : <img className="image" src={image} />}
         </div>
     )
 }
